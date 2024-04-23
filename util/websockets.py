9d1f7ba572312628d8929ce1e7aec0d2b9d5dbeb
j4jet <jeforget@buffalo.edu>
2024-04-23 18:44:21 -0400

    ok now i can actually parse frames

--- util/websockets.py
+++ util/websockets.py
@@ -2,21 +2,17 @@ import base64
 import hashlib
 
 
-def compute_accept(web_str: str):
-    guid = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+def compute_accept(key):
+    magic_number = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+    concat_key = (key + magic_number).encode()
+    sha1_hash = hashlib.sha1(concat_key).digest()
+    accept_key = base64.b64encode(sha1_hash).decode()
+    return accept_key
 
-    # Get the sha1 of the concatenation of web_str and guid
-    base = hashlib.sha1(web_str + guid)
-
-    # Encode with base64
-    sixty4 = base64.b64encode(base)
-
-    # return
-    return sixty4
 
 def parse_ws_frame(frame):
-    output = Frame
-    output.finbit = (frame[0] & 0b10000000) >> 7  # 0 >> 7 = 0, 128 (256?) >> 7 = 1
+    output = Frame()
+    output.fin_bit = (frame[0] & 0b10000000) >> 7  # 0 >> 7 = 0, 128 (256?) >> 7 = 1
     output.opcode = (frame[0] & 0b00001111)
 
     masking_bit = frame[1] & 0b10000000  # either 0 or 128
@@ -40,17 +36,22 @@ def parse_ws_frame(frame):
         mask = frame[:4]
         frame = frame[4:]  # moving it up again
 
-    output.payload = frame
+    payload = bytearray(frame)  # I think this is my problem
 
     if masking_bit != 0:
-        for index in range(len(output.payload)):  # starts at 0, goes to len - 1
-            mask_index = index % 4
+        for index in range(len(payload)):  # starts at 0, goes to len - 1
+            print("index = " + str(index))
+            mask_index = (index + 1) % 4
             if mask_index == 0:
                 mask_index = 4
             mask_index -= 1
-            output.payload[index] = output.payload[index] ^ mask[mask_index]
+            print("mask index = " + str(mask_index))
+            payload[index] = payload[index] ^ mask[mask_index]
+
+    output.payload = bytes(payload)
 
     return output
+
 
 def generate_ws_frame(payload):
     payload_length = len(payload)
@@ -68,6 +69,7 @@ def generate_ws_frame(payload):
         frame.extend(payload_length.to_bytes(8, byteorder="big"))
         frame.extend(payload)
     return bytes(frame)
+
 
 # When you do the handshake is the only time you can authenticate; after that do the while loop
 # then call receive
@@ -97,11 +99,30 @@ class Frame:
 
 
 def test1():
-    byt = b'0123456789'
+    byt = b'jfn948hpf9845gp984h5gnvh45ngm984h5g0984h5go4hfp983h3bf8h3-fp983f3i01234567898y9hfpu4owgfhobubfpq3u948fv1y3pfy8n13948hpv3g4fcgh47fp34hfp348yfno7y3h40jv2ny457t28hmv24578tvn2m4578ntv'
     print(byt)
     byt2 = b'987654321'
 
     byts = b'1234567890'
+
+    rand_str = "Allosaurus-fragilis-Saurophaganax-maxiumus"
+
+    acc = compute_accept(rand_str)
+
+    print(acc)
+
+    frame = bytearray()
+    frame.append(0b10000000)
+    frame.append(0b00000010)
+    frame.append(0b10110100)
+    frame.append(0b01100110)
+
+    parsed = parse_ws_frame(frame)
+
+    print(parsed.payload)
+    print(parsed.fin_bit)
+    print(parsed.payload_length)
+    print(parsed.opcode)
 
     # 1 in binary is 00000001 and 128 is 10000000, so 1 & 128 = 00000000
     # for i in range(len(byt)):
@@ -112,5 +133,62 @@ def test1():
     #         print("no mask")
 
 
+def test2():
+    frame = bytearray()
+    frame.append(0b10000000)
+    frame.append(0b00000010)
+    frame.append(0b10110100)
+    frame.append(0b01100110)
+
+    parsed = parse_ws_frame(frame)
+
+    print(parsed.payload)
+    print(parsed.fin_bit)
+    print(parsed.payload_length)
+    print(parsed.opcode)
+
+
+def test3():
+    frame = bytearray()
+    # finbit of 1, opcode of 8
+    frame.append(0b10001000)
+    # mask of 1, payload len of 4
+    frame.append(0b10000100)
+
+    # mask
+    frame.append(0b10110100)
+    frame.append(0b01100110)
+    frame.append(0b01000010)
+    frame.append(0b01111100)
+
+    # payload:
+    frame.append(0b10010100)
+    frame.append(0b01111110)
+    frame.append(0b01010010)
+    frame.append(0b01000100)
+
+    parsed = parse_ws_frame(frame)
+
+    print(parsed.payload)
+    print(parsed.fin_bit)
+    print(parsed.payload_length)
+    print(parsed.opcode)
+
+    m_1 = 0b10110100 ^ 0b10010100
+    m_2 = 0b01100110 ^ 0b01111110
+    m_3 = 0b01000010 ^ 0b01010010
+    m_4 = 0b01111100 ^ 0b01000100
+
+    payload = bytearray()
+    payload.append(m_1)
+    payload.append(m_2)
+    payload.append(m_3)
+    payload.append(m_4)
+
+    print(parsed.payload)
+    print(bytes(payload))
+    assert parsed.payload == bytes(payload)
+
+
 if __name__ == "__main__":
-    test1()
+    test3()
